a85fd9675b289902f9cf49de2a994dd8a20752e8
valentinedwv <david.valentine@gmail.com>
2016-04-04 15:20:50 -0700

    freetds odbc ini

--- tests/test_SessionFactory.py
+++ tests/test_SessionFactory.py
@@ -8,9 +8,10 @@ from sqlalchemy.engine import reflection
 # [name, driver, connectionstring ]
 dbs_readonly = [
  #   ['mysql', 'localhost', 'odm2', 'ODM', 'odm'],
-     ['mysql@Localhost/odm2', 'mysql', 'mysql+pymysql://root:@localhost/odm2'],
-    [' mysql + mysqldb:', 'mysql', 'mysql+mysqldb://root@localhost/odm2'],
-
+#     ['mysql@Localhost/odm2', 'mysql', 'mysql+pymysql://root:@localhost/odm2'],
+#    [' mysql + mysqldb:', 'mysql', 'mysql+mysqldb://root@localhost/odm2'],
+    ['mysql@Localhost/odm2', 'mysql', 'mysql+pymysql://root:@localhost/'],
+    [' mysql + mysqldb:', 'mysql', 'mysql+mysqldb://root@localhost/'],
                      #'mysql+pymysql://ODM:odm@127.0.0.1/odm2'
      ['postgresql_marchantariats', 'postgresql', 'postgresql+psycopg2://postgres:None@localhost/marchantariats', 'marchantariats', 'postgres',  None],
     #'postgresql+psycopg2://postgres:None@localhost/marchantariats'
