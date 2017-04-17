d1fb40ed562c5584d9184732eacc5fc9c68f56f8
sreeder <stephanie.reeder@usu.edu>
2017-04-17 16:58:56 -0600

    update conn_string to prevent timeout

--- odm2api/ODMconnection.py
+++ odm2api/ODMconnection.py
@@ -17,17 +17,15 @@ class SessionFactory():
     def __init__(self, connection_string, echo=True, version = 2.0):
 
         if 'sqlite' in connection_string:
-            self.engine = create_engine(connection_string,  encoding='utf-8', echo=echo, pool_recycle=1000)
+            self.engine = create_engine(connection_string,  encoding='utf-8', echo=echo, pool_recycle=100)#, pool_pre_ping=True)
             self.test_engine = self.engine
         elif 'mssql' in connection_string:
-            self.engine = create_engine(connection_string, encoding='utf-8', echo=echo, pool_recycle=1000)
-            self.test_engine = create_engine(connection_string, encoding='utf-8', echo=echo, pool_recycle=1000,
-                                             connect_args={'timeout': 1})
+            self.engine = create_engine(connection_string, encoding='utf-8', echo=echo, pool_recycle=100)#, pool_pre_ping =True)
+            self.test_engine = create_engine(connection_string, encoding='utf-8', echo=echo, connect_args={'timeout': 1})
         elif 'postgresql' in connection_string or 'mysql' in connection_string:
-            self.engine = create_engine(connection_string, encoding='utf-8', echo=echo, pool_recycle=1000,
-                                        pool_timeout=1000, pool_size=20, max_overflow=0)
-            self.test_engine = create_engine(connection_string, encoding='utf-8', echo=echo, pool_recycle=1000,
-                                             pool_timeout=5, max_overflow=0, connect_args={'connect_timeout': 1})
+            self.engine = create_engine(connection_string, encoding='utf-8', echo=echo, pool_recycle=100)# , pool_pre_ping=True)
+            self.test_engine = create_engine(connection_string, encoding='utf-8', echo=echo,
+                                              max_overflow=0, connect_args={'connect_timeout': 1})
 
         # Create session maker
         self.Session = scoped_session(sessionmaker(bind=self.engine, autoflush=True))
