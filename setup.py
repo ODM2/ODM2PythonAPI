81ff5fb8654db3cff504b531e93cb36c1e168af0
Filipe <ocefpaf@gmail.com>
2017-05-26 15:26:12 -0400

    Bump to version 0.6.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.5.3.a0',
+    version='0.6.0.a0',
 
     description='A Python-based application programmers interface for the Observations Data converter 2 (ODM2) ',
     long_description=long_description,
