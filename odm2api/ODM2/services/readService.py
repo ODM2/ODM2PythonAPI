b81521d3b24af3c95353e9402522ccd3bdfab9ff
lsetiawan <landungs@uw.edu>
2017-09-27 16:25:32 -0700

    Use google doc style and resolve comments

--- odm2api/ODM2/services/readService.py
+++ odm2api/ODM2/services/readService.py
@@ -543,30 +543,25 @@ class ReadODM2(serviceBase):
             return None
 
     def getAffiliations(self, ids=None, personfirst=None, personlast=None, orgcode=None):
-        """
-        Retrieve a list of ODM2 Affiliation objects.
+        """Retrieve a list of Affiliation objects.
 
-        Parameters
-        ----------
-        ids: list, default None
-            List of AffiliationIDs
-        personfirst: str, default None
-            Person First Name
-        personlast: str, default None
-            Person Last Name
-        orgcode: str, default None
-            Organization Code
+        If no arguments are passed to the function, or their values are None,
+        all Affiliation objects in the database will be returned.
 
-        Returns
-        -------
-        List of Affiliation objects
+        Args:
+            ids (:obj:`list`, optional): List of AffiliationIDs. Defaults to None.
+            personfirst (:obj:`str`, optional): Person First Name. Defaults to None.
+            personlast (:obj:`str`, optional): Person Last Name. Defaults to None.
+            orgcode (:obj:`str`, optional): Organization Code. Defaults to None.
 
-        Examples
-        --------
-        >>> read.getAffiliations(ids=[39,40])
-        >>> read.getAffiliations(personfirst='Anthony',
-        ...                      personlast='Aufdenkampe')
-        >>> read.getAffiliations(orgcode='LimnoTech')
+        Returns:
+            list: List of Affiliation objects
+
+        Examples:
+            >>> read.getAffiliations(ids=[39,40])
+            >>> read.getAffiliations(personfirst='John',
+            ...                      personlast='Smith')
+            >>> read.getAffiliations(orgcode='Acme')
 
         """
         q = self._session.query(Affiliations)
