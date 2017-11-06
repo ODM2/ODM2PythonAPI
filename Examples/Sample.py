959ba641b3ba6d871e408e572bbe54e24b375d97
sreeder <stephanie.reeder@usu.edu>
2017-11-06 14:42:39 -0700

    remove extra info

--- Examples/Sample.py
+++ Examples/Sample.py
@@ -27,7 +27,7 @@ from odm2api.ODM2.services import CreateODM2
 # session_factory = dbconnection.createConnection('sqlite', '/Users/stephanie/DEV/YODA-Tools/tests/test_files/XL_specimen.sqlite', 2.0)
 
 
-session_factory = dbconnection.createConnection('postgresql', 'odm2wofpy1.uwrl.usu.edu', 'odm2', 'dbadmin', 'pinkbananastastegross')
+
 
 
 
@@ -42,7 +42,7 @@ read = ReadODM2(session_factory)
 create = CreateODM2(session_factory)
 
 
-ds= read.getSamplingFeatureDatasets(ids = [1])
+ds= read.getSamplingFeatureDatasets(ids = [1001])
 
 
 # Run some basic sample queries.
