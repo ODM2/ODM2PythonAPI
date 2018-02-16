23891b274691e09d50759d9863b5193d36413291
lsetiawan <landungs@uw.edu>
2018-02-16 12:51:05 -0800

    Fix import flake8 error

--- odm2api/__init__.py
+++ odm2api/__init__.py
@@ -1,13 +1,13 @@
 from __future__ import (absolute_import, division, print_function)
 
 from odm2api.ODMconnection import SessionFactory, dbconnection
-from odm2api.base import serviceBase, modelBase
+from odm2api.base import modelBase, serviceBase
 
 __all__ = [
     'SessionFactory',
     'dbconnection',
+    'modelBase',
     'serviceBase',
-    'modelBase'
 ]
 
 from ._version import get_versions
