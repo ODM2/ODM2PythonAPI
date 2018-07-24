c8dd39debb36d6af64afdad47d12961569073e30
Travis CI User <travis@example.org>
2018-07-24 18:57:35 +0000

    Update docs after building Travis build 585 of ODM2/ODM2PythonAPI
    The docs were built from the branch 'development' against the commit
    de5a61cd0dd1b5682ead763498814243bbf2d12e.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/ODM2/ODM2PythonAPI/jobs/407747506.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/TEST/lib/python3.6/site-packages/doctr/__main__.py deploy --sync --no-require-master --built-docs docs/build/html docs-development

--- docs-development/_static/documentation_options.js
+++ docs-development/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '0.7.0+23.g88bc642',
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '0.7.1+2.gde5a61c',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
