96a3f15bac8336b55f7676c14d3ac8050fa68eed
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-04-21 03:35:56 +0000

    Update docs after building Travis build 604 of ODM2/ODM2PythonAPI
    The docs were built from the branch 'development' against the commit
    6997f36739e7f0ae4d68b984710cb10152a2cc09.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/ODM2/ODM2PythonAPI/jobs/522635233.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/TEST/lib/python3.6/site-packages/doctr/__main__.py deploy --sync --no-require-master --built-docs docs/build/html docs-development

--- docs-development/_static/documentation_options.js
+++ docs-development/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.7.1+9.gd6f3f56',
+    VERSION: '0.7.1+12.g6997f36',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
